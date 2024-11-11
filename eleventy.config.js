8d5454fd92ae2129de04e01eae39574fdadd7ee5
Odhran McGranaghan <odhran.19@gmail.com>
2024-11-11 23:51:06 +0000

    tidy up

---
+++ eleventy.config.js
@@ -0,0 +1,8 @@
+module.exports = async function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",      // Input directory
+      output: "_site",   // Output directory
+    },
+  };
+};
